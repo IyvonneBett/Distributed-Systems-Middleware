48babfc9fa1edef9a19a45472e275dafb044f805
Iyvonne Bett <51298650+IyvonneBett@users.noreply.github.com>
2019-10-01 15:44:06 +0300

    Add files via upload
    The Web application above adds three integer numbers. It will launch ASP.NET Core website into the browser. Then simply add /Home/sum at the end of link and press enter.

---
+++ WebApplication Addition/WebApplication Addition/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SumNumber.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Your application description page.";
+
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			ViewData["Message"] = "Your contact page.";
+
+			return View();
+		}
+
+		public IActionResult Error()
+		{
+			return View();
+		}
+
+		public IActionResult sum()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult add()
+		{
+			int num1 = Convert.ToInt32(HttpContext.Request.Form["txtFirst"].ToString());
+			int num2 = Convert.ToInt32(HttpContext.Request.Form["txtSecond"].ToString());
+			int num3 = Convert.ToInt32(HttpContext.Request.Form["txtThird"].ToString());
+			int result = num1 + num2 + num3;
+			ViewBag.SumResult = result.ToString();
+			return View("sum");
+		}
+	}
+}
